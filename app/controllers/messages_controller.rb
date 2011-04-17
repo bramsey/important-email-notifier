afa076fb607e96d0f7cf8e871bb65a579cc27051
Bill Ramsey <bill@vybly.com>
2011-04-17 18:50:06 -0400

    fixed messages controller auth method

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -53,7 +53,7 @@ class MessagesController < ApplicationController
   private
 
     def authorized_user
-      @user = User.find(params[:id])
-      redirect_to root_path unless current_user?(@user)
+      @message = Message.find(params[:id])
+      redirect_to root_path unless current_user?(@message.recipient)
     end
 end
