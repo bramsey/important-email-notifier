f74dda11b24b13fbcb4746f61dc2e9088236d190
Bill Ramsey <bill@vybly.com>
2011-05-30 18:29:42 -0400

    modularized the listener

--- bin/listener.rb
+++ bin/listener.rb
@@ -3,10 +3,32 @@ require File.join(File.dirname(__FILE__), '..', 'config', 'environment')
 
 RUBY = '/Users/spamram/.rvm/rubies/ruby-1.9.2-p180/bin/ruby'
 
-Account.active_accounts.each do |account|
-  # go through each account and start the daemons.
+# Possibly have the start/stop all functionality within rails and have the start
+# command to be parsed by the listener pass the account info to connect so this
+# script doesn't need to launch a separate rails instance.
+
+def startAll
+  Account.active_accounts.each do |account|
+    # go through each account and start the daemons.
+    start account
+  end
+end
+
+def stopAll
+  Account.all.each do |account|
+    stop account
+  end
+end
+
+def start( account )
   %x[#{RUBY} idle_ctl.rb start #{account.id} -- #{account.username} #{account.password}]
 end
+
+def stop( account )
+  %x[#{RUBY} idle_ctl.rb stop #{account.id}]
+end
+
+startAll
 
 #loop do
 #  sleep 5
