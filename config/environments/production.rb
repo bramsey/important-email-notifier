96f6e19162c4edb56cc975ccc0137d73383ee525
Bill Ramsey <bill@vybly.com>
2011-05-31 17:16:48 -0400

    added idler init to production file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,4 +47,8 @@ Notifier::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   
+  config.after_initialize do
+    IDLER.init
+  end
+  
 end
--- config/initializers/idle_init.rb
+++ config/initializers/idle_init.rb
@@ -1,11 +1,16 @@
 require 'starling'
 
-starling = Starling.new('localhost:22122')
+class IDLER
 
-Account.all.each do |account|
-  if account.active
-    starling.set('idler_queue', "start #{account.id} #{account.username} #{account.password}")
-  else
-    starling.set('idler_queue', "stop #{account.id}")
+  def init
+    starling = Starling.new('localhost:22122')
+
+    Account.all.each do |account|
+      if account.active
+        starling.set('idler_queue', "start #{account.id} #{account.username} #{account.password}")
+      else
+        starling.set('idler_queue', "stop #{account.id}")
+      end
+    end
   end
 end
