9915f986dc4b4b4f327244f7854edfeb79257095
Bill Ramsey <bill@vybly.com>
2011-05-31 16:57:52 -0400

    added initializer file

---
+++ config/initializers/idle_init.rb
@@ -0,0 +1,11 @@
+require 'starling'
+
+starling = Starling.new('localhost:22122')
+
+Account.all.each do |account|
+  if account.active
+    starling.set('idler_queue', "start #{account.id} #{account.username} #{account.password}")
+  else
+    starling.set('idler_queue', "stop #{account.id}")
+  end
+end
